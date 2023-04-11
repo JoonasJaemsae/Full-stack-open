4063e216f7d0781f32c4462f1661093ef973670a
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-04-11 15:10:38 +0300

    Completed 4.1 and 4.2.

---
+++ part4/bloglist/app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config')
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+const mongoUrl = config.MONGODB_URI
+
+logger.info('connecting to', mongoUrl)
+mongoose.connect(mongoUrl)
+    .then(() => {
+        logger.info('connected to MongoDB in bloglist')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB in bloglist:', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
