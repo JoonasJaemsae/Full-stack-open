94f20b7ec57c0660ef267cd3a11e5fdeba5a85f0
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-10-05 17:47:44 +0300

    Implement test 'blogs are returned as json'

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,13 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
