ac76bc4ef7d5f0a8e1673d2f46c78ed740560743
Joonas Jämsä <jamsa.joonas@gmail.com>
2023-10-23 14:43:20 +0300

    Refactor blogs.js to use express-async-errors

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -9,8 +9,15 @@ blogsRouter.get('/', (request, response) => {
         })
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
     const body = request.body
+
+    if (!body.title) {
+        return response.status(400).json({ error: 'Title is missing' })
+    }
+    if (!body.url) {
+        return response.status(400).json({ error: 'URL is missing' })
+    }
 
     const blog = new Blog({
         title: body.title,
@@ -19,12 +26,8 @@ blogsRouter.post('/', (request, response, next) => {
         likes: body.likes || 0,
     })
 
-    blog
-        .save()
-        .then(savedBlog => {
-            response.status(201).json(savedBlog)
-        })
-        .catch(error => next(error))
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
